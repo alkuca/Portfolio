904090936bb0704e219d16d003d83311294c6f9c
alen commit <alkuca@gmail.com>
2018-08-15 17:55:17 +0200

    marker changes

--- src/Loader.js
+++ src/Loader.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import Background from './back2.svg';
 class Loader extends Component {
+    state=({
+       loaded:false
+    });
+
+    componentDidMount(){
+        this.setState({
+            loaded:true
+        })
+    }
 
 
     render() {
         return (
+            this.state.loaded ? null :
                 <div className="loader">
                     <div className="loader-img-wrapper">
                         <img id="loader-logo" alt="loader" src={Background}/>
